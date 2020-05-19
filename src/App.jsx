b055ed7d374bb0f95a24b2acc80ccc98ba9fbbdf
parashardhapola <parashar.dhapola@gmail.com>
2020-05-19 11:54:52 +0200

    back to scinet domain

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { SubmitButton, SvgDownload } from "./components.jsx"
 import { rgb } from "d3-color";
 import * as Radar from "./radar.js"
 
-const SERVER = "https://130.235.249.179/cellradar"
+const SERVER = "https://sci-net.org/cellradar"
 
 class App extends React.Component {
     constructor () {
